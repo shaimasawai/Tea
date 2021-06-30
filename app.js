312e1bd925485c3558e6dd23986fbba940e8759f
shaimasawai <>
2021-06-30 12:31:30 +0000

    type of tea

---
+++ app.js
@@ -0,0 +1,23 @@
+
+var firstname = " shaima"
+var lastname = " sawaie"
+console.log ( firstname + lastname)
+var x=1
+console . log("1")
+console . log ("hello")
+
+alert (" welcom to my websit")
+var ask = prompt("you like tea")
+var x = "yes"
+var y="No"
+if (ask == "yes")
+
+ var ask = prompt (" what is your favorit type of tea")
+
+ else
+ 
+ alert (" its sad")
+
+
+
+
