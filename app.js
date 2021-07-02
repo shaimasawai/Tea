9e7477c6955e04634fb8f55779cd93df5476037a
shaimasawai <>
2021-07-02 19:04:21 +0000

    loop

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 
-var firstname = " shaima"
-var lastname = " sawaie"
-console.log ( firstname + lastname)
-var x=1
-console . log("1")
-console . log ("hello")
+//var firstname = " shaima"
+//var lastname = " sawaie"
+//console.log ( firstname + lastname)
+//var x=1
+//console . log("1")
+//console . log ("hello")
 
-alert (" welcom to my websit")
-var ask = prompt("you like tea")
-var x = "yes"
-var y="No"
-if (ask == "yes")
+//alert (" welcom to my websit")
+//var ask = prompt("you like tea")
+//var x = "yes"
+//var y="No"
+//if (ask == "yes")
 
- var ask = prompt (" what is your favorit type of tea")
+ //var ask = prompt (" what is your favorit type of tea")
 
- else
+// else
  
- alert (" its sad")
-document.write ('<h3>'+ ask + '</h3>');
+// alert (" its sad")
+//document.write ('<h3>'+ ask + '</h3>');
 
+//var guessname= prompt("guessnam")
+////if (guessname == 3)
+////{
 
+ // alert ("you got the fiirst guess right")
+//}
+//else
+//alert (" thanks")
 
+var result = prompt("How many words are in the paragraph?"); 
+    while (result<190||result>200||result==isNaN(result)) 
+    {
+        var result = prompt("You need to give number How many words are in the paragraph?!");
+    }
+  alert ("success")
